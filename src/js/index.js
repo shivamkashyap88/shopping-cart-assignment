142041588fcdc8689cc59b02a3369dfe50e34112
shivamkashyap88 <shivamkashyap18@gmail.com>
2021-05-07 12:53:48 +0530

    Update index html

--- src/js/index.js
+++ src/js/index.js
@@ -56,62 +56,38 @@ window.addCustomClass = function () {
 }
 
 var generateHTMLProduct = function (products) {
-    var product = document.createElement("div");
-    var row = document.createElement("div");
-    var product_flex = document.createElement("div");
-    var image_div = document.createElement("div");
-    var product_column = document.createElement("div");
-    var product_flex_grow = document.createElement("div");
-    var smallscr = document.createElement("div");
-    var largscr = document.createElement("div");
-    var span = document.createElement("span");
-    var button = document.createElement("button");
-    var button2 = document.createElement("button");
-    var lrgbutton = document.createElement("button");
-    var img = document.createElement("img");
-    var p = document.createElement("p");
-    var h2 = document.createElement("h2");
-    product.className = "product";
-    row.className = "product-row";
-    product_flex.className = "product__display-flex";
-    product_column.className = "product__display-column";
-    product_flex_grow.className = "product__flex-grow";
-    span.className = "product__button-row";
-    image_div.className = "product__img";
-    largscr.className = "btn-large-screen";
-    smallscr.className = "btn-small-screen";
-    button.className = "btn";
-    button2.className = "btn product__button-price";
-    lrgbutton.className = "btn product__button-price2";
-    img.src = products.imageURL;
-    p.innerHTML = products.description;
-    button.innerHTML = "Buy Now @ Rs." + products.price;
-    button.setAttribute('onclick', `addProduct("${products.id}")`);
-    lrgbutton.innerHTML = "Buy Now @ Rs." + products.price;
-    lrgbutton.setAttribute('onclick', `addProduct("${products.id}")`);
-    h2.innerHTML = products.name;
-    span.innerHTML = "MRP Rs." + products.price;
-    button2.innerHTML = "Buy Now";
-    button2.setAttribute('onclick', `addProduct("${products.id}")`);
-    image_div.appendChild(img);
-    smallscr.appendChild(button);
-    product_flex_grow.appendChild(p);
-    product_column.appendChild(product_flex_grow);
-    product_column.appendChild(smallscr);
-    product_flex.appendChild(image_div);
-    product_flex.appendChild(product_column);
-    largscr.appendChild(span);
-    largscr.appendChild(lrgbutton);
-    largscr.appendChild(button2);
-    row.appendChild(h2);
-    row.appendChild(product_flex);
-    row.appendChild(largscr);
-    product.appendChild(row);
-    document.getElementsByClassName("products-list")[0].appendChild(product);
+    var template = `
+    <section class="product">
+        <div class="product-row">
+            <h2>${products.name}</h2>
+            <article class="product__display-flex">
+                    <figure class="product__img">
+                        <img src="${products.imageURL}">
+                    </figure>
+                <div class="product__display-column">
+                    <div class="product__flex-grow">
+                        <p>${products.description}</p>
+                    </div>
+                    <div class="btn-small-screen">
+                        <button class="btn" onclick=` + `"addProduct('${products.id}')">`+`${"Buy Now @ Rs." + products.price}</button>
+                    </div>
+                </div>
+            </article>
+            <div class="btn-large-screen">
+                <span class="product__button-row">${"MRP Rs." + products.price}</span>
+                <button class="btn product__button-price2" onclick=`+`"addProduct('${products.id}')">`+ `${"Buy Now @ Rs." + products.price}</button>
+                <button class="btn product__button-price" onclick=` +`"addProduct('${products.id}')">`+ `Buy Now</button>
+            </div>
+        </div>
+    </section>
+    `;
+    document.getElementsByClassName("products-list")[0].insertAdjacentHTML('beforeend',template);
 }
+
 window.renderHeader = function () {
     document.getElementsByTagName('body')[0].insertAdjacentHTML('afterbegin', header.renderHeader());
 }
+
 window.renderProducts = function (id) {
     for (var i = 0; i < products.length; i++) {
         if (id && products[i].category === id) {
